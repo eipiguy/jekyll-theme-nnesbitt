9905fad33d2da5bfd066d0ce57f59f4ab1ad615c
Neal Nesbitt <ndnesbitt@dnesbitt.com>
2023-09-03 11:34:55 -0700

    haha! animation on button click!

--- docs/imagetournament/dragImageGrid.js
+++ docs/imagetournament/dragImageGrid.js
@@ -16,6 +16,73 @@ class DragImageGrid {
 		return (row * this.cols) + col;
 	}
 
+	computeCoords( start_coords ) {
+		let cell_coords = [];
+		for (let i = 0; i < this.cell_rows; i++) {
+			for (let j = 0; j < this.cols; j++) {
+				cell_coords[this.rowColToIndex(i,j)] = {
+					x: start_coords[0] + (j * this.cell_size),
+					y: start_coords[1] + (i * this.cell_size)
+				};
+			}
+		}
+		return cell_coords
+	}
+
+	fillFromNext(i) {
+		const next_id = this.nextFilledStack(i);
+		if (next_id > 0) {
+			this.image_stacks[i].push(this.image_stacks[next_id].pop());
+		}
+	}
+
+	nextFilledStack(i) {
+		for (let j = i + 1; j < this.image_stacks.length; j++) {
+			if (this.image_stacks[j].length > 0) {
+				return j;
+			}
+		}
+		return -1;
+	}
+
+	pushToNext(i) {
+		if (i + 1 < this.image_stacks.length) {
+			this.image_stacks[i + 1].push(this.image_stacks[i].shift());
+		}
+	}
+
+	correctStackSize(stack_id) {
+		let stack_size = this.image_stacks[stack_id].length
+		if (stack_size < this.min_stack_size) {
+			this.fillFromNext(stack_id);
+		} else if (stack_size > this.max_stack_size) {
+			this.pushToNext(stack_id);
+		}
+		return this.image_stacks[stack_id].length
+	}
+
+	copyTopLayer() {
+		let stack_size = 0;
+		let img_id = -1;
+		let layer_copy = [];
+		for( let i = 0; i < this.num_cells; ++i ) {
+			img_id = this.cell_start_id + i;
+			if( img_id < 0 ) {
+				i = -1-this.cell_start_id;
+				continue;
+			} else if ( img_id >= this.num_imgs ) {
+				break;
+			}
+
+			stack_size = this.correctStackSize(img_id);
+
+			if( stack_size > 0 ) {
+				layer_copy.push( this.image_stacks[img_id][stack_size - 1] )
+			}
+		}
+		return layer_copy;
+	}
+
 	constructor(sketch, relative_paths, container_width, container_height = container_width, columns = 4, draggable = DEFAULT_DRAGGABLE) {
 		this.sketch = sketch;
 
@@ -34,17 +101,16 @@ class DragImageGrid {
 			this.image_stacks.push([sketch.loadImage('/docs/midjourney/midjourney/' + relative_paths[i])]);
 		}
 
-		this.cell_coords = [];
-		for (let i = 0; i < this.cell_rows; i++) {
-			for (let j = 0; j < this.cols; j++) {
-				this.cell_coords[this.rowColToIndex(i,j)] = {
-					x: j * this.cell_size,
-					y: i * this.cell_size
-				};
-			}
-		}
-
 		this.cell_start_id = 0;
+		this.cell_start_coords = [0,0];
+		this.cell_coords = this.computeCoords( this.cell_start_coords );
+
+		this.transition_steps = 8;
+		this.is_animating = false;
+		this.animation_progress = 0;
+		this.animation_direction = 0; // -1 for up, 1 for down, 0 for no movement
+		this.animation_step = this.cell_size / this.transition_steps;
+
 		this.min_stack_size = 1;
 		this.max_stack_size = 1;
 		this.draggable = draggable;
@@ -68,15 +134,10 @@ class DragImageGrid {
 		}
 		const cell_change = this.checkButtonClicked(y);
 		if( cell_change != 0 ) {
-			this.cell_start_id += cell_change * this.cols;
-			if( this.cell_start_id < 0 ){
-				this.cell_start_id = 0;
-			}else if( this.cell_start_id >= Math.max(0, this.num_imgs - this.num_cells)) {
-				this.cell_start_id = Math.max(0, this.num_imgs - this.num_cells);
-			}
-			else {
-				return;
-			}
+			this.is_animating = true;
+			this.animation_direction = cell_change;
+			// Skip rest of touch functionality under button
+			return;
 		}
 
 		const cell_index = this.xyToCellIndex(x, y);
@@ -170,64 +231,64 @@ class DragImageGrid {
 	}
 
 	draw() {
-		let num_stack_imgs = 0;
-		let img_id = -1;
-		for (let i = 0; i < this.num_cells; i++) {
-			img_id = i+this.cell_start_id;
-			if( img_id < 0 || img_id >= this.num_imgs ){
-				continue;
-			}
 
-			num_stack_imgs = this.image_stacks[img_id].length;
-			if (num_stack_imgs < this.min_stack_size) {
-				this.fillFromNext(img_id);
-			} else if (num_stack_imgs > this.max_stack_size) {
-				this.pushToNext(img_id);
-			}
+		if (this.is_animating) {
+			this.animation_progress += 1; // You can adjust this rate
+			let dy = this.animation_direction * this.animation_step * this.animation_progress;
+			this.drawLayer(this.cell_start_id, [this.cell_start_coords[0], dy]);
+	
+			if (Math.abs(dy) >= this.cell_size) {
+				this.is_animating = false;
+				this.animation_progress = 0;
+				this.cell_start_id -= this.animation_direction * this.cols;
 
-			this.drawCell(i, img_id);
+				if( this.cell_start_id < - this.cols ) {
+					this.cell_start_id = -this.cols;
+				} else if( this.cell_start_id + this.num_cells - this.cols > this.num_imgs ) {
+					this.cell_start_id = this.num_imgs - this.num_cells + this.cols;
+				}
+			}
+		} else {
+			// Draw current top layer of image stacks
+			this.drawLayer();
 		}
+
+		// Draw active dragged images
 		for (const id in this.active_touches) {
 			this.drawTouch(id);
 		}
 	}
 
-	fillFromNext(i) {
-		const next_id = this.nextFilledStack(i);
-		if (next_id > 0) {
-			this.image_stacks[i].push(this.image_stacks[next_id].pop());
-		}
-	}
-
-	nextFilledStack(i) {
-		for (let j = i + 1; j < this.image_stacks.length; j++) {
-			if (this.image_stacks[j].length > 0) {
-				return j;
+	drawLayer( start_id = this.cell_start_id, start_coords = this.cell_start_coords) {
+		let img_id = -1;
+		for (let i = 0; i < this.num_cells; i++) {
+			img_id = start_id + i;
+			if( img_id < 0 ) {
+				i = -1-start_id;
+				continue;
+			} else if ( img_id >= this.num_imgs ) {
+				return;
 			}
-		}
-		return -1;
-	}
 
-	pushToNext(i) {
-		if (i + 1 < this.image_stacks.length) {
-			this.image_stacks[i + 1].push(this.image_stacks[i].shift());
+			this.correctStackSize(img_id);
+			this.drawCell( i, img_id, start_coords[0], start_coords[1] );
 		}
 	}
 
-	drawCell(cell_index, image_index) {
+	drawCell(cell_index, image_index, dx = 0, dy = 0) {
 		if (this.image_stacks[image_index].length > 0) {
 			this.sketch.image(
 				this.image_stacks[image_index][this.image_stacks[image_index].length - 1],
-				this.cell_coords[cell_index].x,
-				this.cell_coords[cell_index].y,
+				this.cell_coords[cell_index].x + dx,
+				this.cell_coords[cell_index].y + dy,
 				this.cell_size,
 				this.cell_size
 			);
 		}
 	}
 
-	drawTouch(id) {
-		const touch = this.active_touches[id];
+	drawTouch(image_index) {
+		const touch = this.active_touches[image_index];
 		this.sketch.image(
 			touch.drag_image,
 			touch.drag_image_x,
